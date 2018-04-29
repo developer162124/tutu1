416bc264327ae886a0e7ef917b3660ffa1f196e4
developer <developer172124@yandex.ru>
2018-04-30 04:26:21 +0700

    Модели регистрации/авторизации/index + templates

--- tutu_core/tutu_service/views.py
+++ tutu_core/tutu_service/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    if request.user.is_authenticated():
+        context['auth'] = request.user
+    return render(request, "tutu_service/index.html", context)
+
+
+def register(request):
+    if request.POST:
+        user = User.objects.create_user(
+                password=request.POST["pwd"],
+                username=request.POST["name"],
+        )
+        user.save()
+        return redirect(reverse('tutu_service:index'))
+    else:
+        return render(request, "tutu_service/register.html")
+
+
+def login(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            return redirect(reverse('tutu_service:index'))
+        else:
+            return redirect(reverse('tutu_service:index'))
+    else:
+        return render(request, "tutu_service/login.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('tutu_service:index'))
